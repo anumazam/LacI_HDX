72c0d87a66a0dbbfc48b0018a816815e2edafb74
Anum Glasgow <anumazam@gmail.com>
2021-04-11 10:44:21 -0700

    set better start vals on fit function

--- color_peptide.py
+++ color_peptide.py
@@ -8,10 +8,10 @@ os.chdir('/Users/anumglasgow/Dropbox/Research/Data/HDX/20200922_compile_rates')
 
 input_structure = "2P9H"
 
-cmd.fetch(input_structure)
+#cmd.fetch(input_structure)
 
 def color_peptide(first_pos, last_pos):
-    cmd.orient()
+#    cmd.orient()
     peptide = "resi " + first_pos + "-" + last_pos
     cmd.color("yellow",peptide)
 
